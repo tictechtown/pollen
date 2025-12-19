fdc133f4cd8e5792446236f8492f605013599b65
tictechtown <tictechtown@gmail.com>
2025-12-19 16:08:28 -0500

    Cleanup

--- store/filters.ts
+++ store/filters.ts
@@ -3,9 +3,7 @@ import { create } from 'zustand'
 type FiltersState = {
   selectedFeedId?: string
   selectedFeedTitle?: string
-  showUnseenOnly: boolean
   setFeedFilter: (feedId?: string, title?: string) => void
-  setShowUnseenOnly: (value: boolean) => void
   clear: () => void
 }
 
@@ -14,11 +12,9 @@ export const useFiltersStore = create<FiltersState>((set) => ({
   selectedFeedTitle: undefined,
   showUnseenOnly: false,
   setFeedFilter: (selectedFeedId, selectedFeedTitle) => set({ selectedFeedId, selectedFeedTitle }),
-  setShowUnseenOnly: (showUnseenOnly) => set({ showUnseenOnly }),
   clear: () =>
     set({
       selectedFeedId: undefined,
       selectedFeedTitle: undefined,
-      showUnseenOnly: false,
     }),
 }))
